eaffcc1966f8b72f84b73041cc3663399b0ae72b
AdrielleSoares <adriellecspsoares@hotmail.com>
2022-10-06 16:25:14 -0300

    Exercicios em Javascript

---
+++ Aula de exercicios em Javascript/exercicio03/script.js
@@ -0,0 +1,8 @@
+
+var numero = document.getElementsByName("numero")
+
+function GerarTabuada(){
+    if(i = numero; i <= 1; i++){
+        
+    }
+}
---
+++ Aula de exercicios em Javascript/modelodeexercicios/script.js
@@ -0,0 +1,8 @@
+let numero = 8
+    if (numero == 7) {
+        console.log('Numero e igual a 7')
+    } else if (numero == 8){
+        console.log('Numero e igual a 8')
+    } else {
+        console.log('Numero e diferente de 7 e de 8')
+    }
